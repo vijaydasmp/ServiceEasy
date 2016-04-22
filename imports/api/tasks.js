0ca18a594a4f37b0a882c2c6c9f97080c2a025a3
Vijay das Manikpuri <vijaydas.mp@gmail.com>
2016-04-22 15:31:23 +0530

    pp

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,12 +1,3 @@
 import { Mongo } from 'meteor/mongo';
  
-myCollection = new Mongo.Collection('myCollection');
-
-var myData = {
-   key1: "Task 1...",
-   key2: "Task 2...",
-   key3: "Task 3...",
-   key4: "Task 4...",
-   key5: "Task 5..."
-}
-myCollection.insert(myData);
+export const myCollection = new Mongo.Collection('myCollection');
